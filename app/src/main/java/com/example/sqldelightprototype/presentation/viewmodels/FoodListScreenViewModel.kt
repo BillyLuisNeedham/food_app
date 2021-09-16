af05570eb244feeb8cc5cb07188fbf46efe4acca
Billy Needham <56001580+BillyLuisNeedham@users.noreply.github.com>
2021-09-16 16:43:27 +0100

    FoodListScreenViewModel.kt builld out

--- app/src/main/java/com/example/sqldelightprototype/presentation/viewmodels/FoodListScreenViewModel.kt
+++ app/src/main/java/com/example/sqldelightprototype/presentation/viewmodels/FoodListScreenViewModel.kt
@@ -2,8 +2,10 @@ package com.example.sqldelightprototype.presentation.viewmodels
 
 import androidx.lifecycle.ViewModel
 import com.example.sqldelightprototype.domain.usecases.GetAllFoodsUseCase
+import dagger.hilt.android.lifecycle.HiltViewModel
 import javax.inject.Inject
 
+@HiltViewModel
 class FoodListScreenViewModel @Inject constructor(
     private val getAllFoodsUseCase: GetAllFoodsUseCase
 ): ViewModel() {
