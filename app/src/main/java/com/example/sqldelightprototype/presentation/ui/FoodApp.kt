8de84160ee4ba7db6c7766e4e84c921d51598d06
Billy Needham <56001580+BillyLuisNeedham@users.noreply.github.com>
2021-09-16 17:22:10 +0100

    basic build out

--- app/src/main/java/com/example/sqldelightprototype/presentation/ui/FoodApp.kt
+++ app/src/main/java/com/example/sqldelightprototype/presentation/ui/FoodApp.kt
@@ -1,23 +1,14 @@
 package com.example.sqldelightprototype.presentation.ui
 
 import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material.Button
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Scaffold
-import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.ExperimentalComposeUiApi
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.navigation.compose.rememberNavController
-import com.example.sqldelightprototype.FoodData
-import com.example.sqldelightprototype.domain.ResultOf
 import com.example.sqldelightprototype.presentation.ui.navigation.AppNavigation
-import com.example.sqldelightprototype.presentation.ui.screens.AddFoodScreen
 import com.example.sqldelightprototype.presentation.ui.theme.SqlDelightPrototypeTheme
 
 @ExperimentalComposeUiApi
--- app/src/main/java/com/example/sqldelightprototype/presentation/ui/MainActivity.kt
+++ app/src/main/java/com/example/sqldelightprototype/presentation/ui/MainActivity.kt
@@ -4,11 +4,7 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.ui.ExperimentalComposeUiApi
-import androidx.lifecycle.lifecycleScope
-import com.example.sqldelightprototype.FoodData
-import com.example.sqldelightprototype.data.localdatasource.FoodDatabase
 import dagger.hilt.android.AndroidEntryPoint
-import kotlinx.coroutines.launch
 
 @AndroidEntryPoint
 class MainActivity : ComponentActivity() {
