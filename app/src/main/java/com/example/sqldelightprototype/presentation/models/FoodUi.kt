93aed25b26a974308b06e3d3bd364cce91d5cd7f
Billy Needham <56001580+BillyLuisNeedham@users.noreply.github.com>
2021-09-20 10:54:52 +0100

    FoodUi.kt build out

---
+++ app/src/main/java/com/example/sqldelightprototype/presentation/models/FoodUi.kt
@@ -0,0 +1,10 @@
+package com.example.sqldelightprototype.presentation.models
+
+
+data class FoodUi(
+    val id: Long,
+    val name: String,
+    val quantity: Int,
+    val expirationMessage: String
+)
+
